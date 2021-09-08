aac788d5c1e4ec60a402b9a955133831d15821a6
Kristaps Kaupe <kristaps@blogiem.lv>
2021-09-08 23:18:12 +0300

    Fix functional tests for Bitcoin Core v0.20+

--- tests/functional/inc.setup.sh
+++ tests/functional/inc.setup.sh
@@ -10,6 +10,9 @@ retval=0
 $bitcoind -daemon || exit 1
 # Wait until bitcoind has started properly
 while ! $bitcoin_cli getblockchaininfo &> /dev/null; do sleep 0.1; done
+# Create and load wallet
+$bitcoin_cli createwallet tests
+$bitcoin_cli loadwallet tests
 # Generate some coins
 $bitcoin_cli generatetoaddress 1 "$($bitcoin_cli getnewaddress "" "legacy")"
 $bitcoin_cli generatetoaddress 1 "$($bitcoin_cli getnewaddress "" "legacy")"
